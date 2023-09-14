14c8f4f0b8be64fe96df54701f3a7861350f9ce6
Gabriel <gabrielferreiracunha@gmail.com>
2023-09-14 20:37:13 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Config/ContextBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Produto;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();                  
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        public DbSet<ProdutoViewModel> ProdutoViewModel { get; set; }
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=LAPTOP-A6EENSJ3\\SQLEXPRESS;Initial Catalog=ApiCoreCamadas;Integrated Security=True";
+            return strCon;
+        }
+    }
+}
